2d6701e37da089c7c4ccff624c6b0b9c310226f1
xpakx <55586470+xpakx@users.noreply.github.com>
2021-06-01 19:00:06 +0200

    chore(backend): delete unneccesaary dependency

--- backend/src/main/java/io/github/xpakx/spotifycollage/controller/MainController.java
+++ backend/src/main/java/io/github/xpakx/spotifycollage/controller/MainController.java
@@ -1,11 +1,5 @@
 package io.github.xpakx.spotifycollage.controller;
 
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.SpotifyHttpManager;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import io.github.xpakx.spotifycollage.model.AuthAddressResponse;
 import io.github.xpakx.spotifycollage.service.SpotifyAuthService;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.URI;
 
 @RestController
 public class MainController {
@@ -23,12 +16,6 @@ public class MainController {
     private String clientId = "";
     @Value("${spotify.client-secret}")
     private String clientSecret = "";
-
-    private SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId(clientId)
-            .setClientSecret(clientSecret)
-            .setRedirectUri(SpotifyHttpManager.makeUri("http://192.168.1.204:8080/redirect"))
-            .build();
 
     private final SpotifyAuthService spotifyAuthService;
 
@@ -45,13 +32,6 @@ public class MainController {
 
     @GetMapping("redirect")
     public String getSpotifyUserCode(@RequestParam("code") String userCode) throws IOException {
-       AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(userCode).build();
-        try{
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
-
-        } catch(IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
-
-        }
-        return spotifyApi.getAccessToken();
+       return spotifyAuthService.requestTokens(userCode);
     }
 }
