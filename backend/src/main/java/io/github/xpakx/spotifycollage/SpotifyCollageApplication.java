0d129e0a557c27fb37f61c29a8d259e1420a80ba
xpakx <55586470+xpakx@users.noreply.github.com>
2021-06-01 13:44:28 +0200

    init

---
+++ backend/src/main/java/io/github/xpakx/spotifycollage/SpotifyCollageApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.spotifycollage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCollageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCollageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/xpakx/spotifycollage/SpotifyCollageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.spotifycollage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCollageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
